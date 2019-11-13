e8219911cfc2f13ed6d18cd83257b7e2052be4c8
humerashaheen2512 <humera.shaheen_cs17@gla.ac.in>
2019-11-13 12:41:42 +0530

    added image button var in Java code

--- app/src/main/java/in/ac/gla/miniproject/womensafetyapp/MainActivity.java
+++ app/src/main/java/in/ac/gla/miniproject/womensafetyapp/MainActivity.java
@@ -1,14 +1,18 @@
 package in.ac.gla.miniproject.womensafetyapp;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageButton helpButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        helpButton = findViewById(R.id.btnHelp);
     }
 }
