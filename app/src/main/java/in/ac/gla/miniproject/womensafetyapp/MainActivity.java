bf7dbffe019f999f028250c566e254d4a99d53c9
humerashaheen2512 <humera.shaheen_cs17@gla.ac.in>
2019-11-14 23:33:40 +0530

    Id of buttons added to Main Activity.

--- app/src/main/java/in/ac/gla/miniproject/womensafetyapp/MainActivity.java
+++ app/src/main/java/in/ac/gla/miniproject/womensafetyapp/MainActivity.java
@@ -1,18 +1,24 @@
 package in.ac.gla.miniproject.womensafetyapp;
 
 import android.os.Bundle;
-import android.widget.ImageButton;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnAdd;
+    Button btnTemplate;
 
-    private ImageButton helpButton;
+    ImageView helpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         helpButton = findViewById(R.id.btnHelp);
+        btnAdd = findViewById(R.id.btnAdd);
+        btnTemplate = findViewById(R.id.btnTemplate);
+
     }
 }
