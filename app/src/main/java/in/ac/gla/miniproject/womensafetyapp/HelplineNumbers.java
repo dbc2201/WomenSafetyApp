f6f89380e807eca7c9f00f0e998f41f884f6fbc8
naman45678 <naman.mangal_cs17@gla.ac.in>
2019-11-15 16:21:58 +0530

    added Helpline java class

---
+++ app/src/main/java/in/ac/gla/miniproject/womensafetyapp/HelplineNumbers.java
@@ -0,0 +1,19 @@
+package in.ac.gla.miniproject.womensafetyapp;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HelplineNumbers extends AppCompatActivity {
+
+    ArrayAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_helpline_numbers);
+
+
+    }
+}
