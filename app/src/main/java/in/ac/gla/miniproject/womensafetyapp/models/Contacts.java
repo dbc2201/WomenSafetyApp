f70c08cf28d63eb0c1cc77bd3dfa1c0fa9b68bf5
humerashaheen2512 <humera.shaheen_cs17@gla.ac.in>
2019-11-18 19:41:22 +0530

    Contact java class added to take contact number and name

--- app/src/main/java/in/ac/gla/miniproject/womensafetyapp/models/Contacts.java
+++ app/src/main/java/in/ac/gla/miniproject/womensafetyapp/models/Contacts.java
@@ -1,4 +1,23 @@
 package in.ac.gla.miniproject.womensafetyapp.models;
 
 public class Contacts {
+    private String name;
+    private String number;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
 }
+
